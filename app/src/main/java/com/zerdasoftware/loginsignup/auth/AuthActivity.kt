b6a0dc3ac03e11d6f3bda6b16cc39174136625ab
ccall <c.calli@diten.tech>
2023-01-19 17:03:11 +0300

    Login Screen Design

---
+++ app/src/main/java/com/zerdasoftware/loginsignup/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.zerdasoftware.loginsignup.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zerdasoftware.loginsignup.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
